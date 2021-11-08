66c193c3e5840e0ab3b2b86f0bd075956dcc5ccd
Raquel Leão <vc.raquel@gmail.com>
2021-11-08 19:00:51 -0300

    cadastrar model

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
@@ -10,5 +10,23 @@ public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
 
+    public Cadastro cadastrarModel(CadastroDTO cadastroDTO) {
+        Cadastro cadastro = new Cadastro();
+        //calculando a data automaticamente
+        LocalDate dataAtual = LocalDate.now();
 
+        cadastro.setBairro(cadastroDTO.getBairro());
+        cadastro.setCidade(cadastroDTO.getCidade());
+        cadastro.setCpf(cadastroDTO.getCpf());
+        cadastro.setIdade(cadastroDTO.getIdade());
+        cadastro.setNome(cadastroDTO.getNome());
+        cadastro.setMoraSozinho(cadastroDTO.isMoraSozinho());
+        cadastro.setTemPet(cadastroDTO.isTemPet());
+        cadastro.setNomeDoParenteProximo(cadastroDTO.getNomeDoParenteProximo());
+        cadastro.setSobrenome(cadastroDTO.getSobrenome());
+        //adicionando a data calculada automaticamente
+        cadastro.setDataDoCadastro(dataAtual);
+
+        return cadastro;
+    }
 }
