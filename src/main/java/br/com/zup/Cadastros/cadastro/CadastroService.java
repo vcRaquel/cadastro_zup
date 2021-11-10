234bb644dca7999befa90164b9de280416b145db
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 09:29:58 -0300

    modificar método de salvar cadastro na service recebendo a model

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
@@ -12,30 +12,37 @@ public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
 
-    public Cadastro cadastrarModel(CadastroDTO cadastroDTO) {//Cadastro cadastro
-        Cadastro cadastro = new Cadastro();
-        //calculando a data automaticamente
-        LocalDate dataAtual = LocalDate.now();
-
-        cadastro.setCpf(cadastroDTO.getCpf());
-        cadastro.setNome(cadastroDTO.getNome());
-        cadastro.setSobrenome(cadastroDTO.getSobrenome());
-        cadastro.setCidade(cadastroDTO.getCidade());
-        cadastro.setBairro(cadastroDTO.getBairro());
-        cadastro.setNomeDoParenteProximo(cadastroDTO.getNomeDoParenteProximo());
-        cadastro.setMoraSozinho(cadastroDTO.isMoraSozinho());
-        cadastro.setTemPet(cadastroDTO.isTemPet());
-        cadastro.setIdade(cadastroDTO.getIdade());
-
-        //adicionando a data calculada automaticamente
-        cadastro.setDataDoCadastro(dataAtual);
-
-        return cadastro;
+    public Cadastro salvarCadastro(Cadastro cadastro){
+        //colocando a data atual no cadastro
+        cadastro.setDataDoCadastro(LocalDate.now());
+        //salvando a entidade no repository?
+        return cadastroRepository.save(cadastro);
     }
 
-    public Cadastro cadastrarNoBanco(CadastroDTO cadastroDTO) {
-        return cadastroRepository.save(cadastrarModel(cadastroDTO));
-    }
+//    public Cadastro cadastrarModel(CadastroDTO cadastroDTO) {//Cadastro cadastro
+//        Cadastro cadastro = new Cadastro();
+//        //calculando a data automaticamente
+//        LocalDate dataAtual = LocalDate.now();
+//
+//        cadastro.setCpf(cadastroDTO.getCpf());
+//        cadastro.setNome(cadastroDTO.getNome());
+//        cadastro.setSobrenome(cadastroDTO.getSobrenome());
+//        cadastro.setCidade(cadastroDTO.getCidade());
+//        cadastro.setBairro(cadastroDTO.getBairro());
+//        cadastro.setNomeDoParenteProximo(cadastroDTO.getNomeDoParenteProximo());
+//        cadastro.setMoraSozinho(cadastroDTO.isMoraSozinho());
+//        cadastro.setTemPet(cadastroDTO.isTemPet());
+//        cadastro.setIdade(cadastroDTO.getIdade());
+//
+//        //adicionando a data calculada automaticamente
+//        cadastro.setDataDoCadastro(dataAtual);
+//
+//        return cadastro;
+//    }
+//
+//    public Cadastro cadastrarNoBanco(CadastroDTO cadastroDTO) {
+//        return cadastroRepository.save(cadastrarModel(cadastroDTO));
+//    }
 
     public List<Cadastro> exibirTodosCadastros(Boolean moraSozinho, Boolean temPet, Integer idade) {
         if (moraSozinho != null) {
